e1a9b6e37227267f00031f3d2a01bc1f3b4f9fa8
Papazy <fajryjobs@gmail.com>
2024-01-25 21:18:57 +0700

    menambah seeder polis

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Poli;
 use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
@@ -16,13 +17,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'name' => 'admin',
-            'role' => 'admin',
-            'email' => 'admin@gmail.com',
-            'is_admin' => 1,
-            'is_superadmin' => 1,
-            'password' => Hash::make('admin'),
-        ]);
+        // User::create([
+        //     'name' => 'admin',
+        //     'role' => 'admin',
+        //     'email' => 'admin@gmail.com',
+        //     'is_admin' => 1,
+        //     'is_superadmin' => 1,
+        //     'password' => Hash::make('admin'),
+        // ]);
+        $polis = [
+            ['name' => 'Poli Umum'],
+            ['name' => 'Poli Bedah'],
+            ['name' => 'Poli Anak'],
+            ['name' => 'Poli Kandungan dan Obstetri'],
+            ['name' => 'Poli Gigi'],
+            ['name' => 'Poli Mata'],
+            ['name' => 'Poli Jantung'],
+            ['name' => 'Poli Kulit dan Kelamin'],
+            ['name' => 'Poli Psikiatri'],
+            ['name' => 'Poli THT (Telinga, Hidung, Tenggorokan)'],
+            ['name' => 'Poli Gizi'],
+            ['name' => 'Poli Diabetes'],
+            ['name' => 'Poli Rehabilitasi'],
+            ['name' => 'Poli Kesehatan Jiwa'],
+            ['name' => 'Poli Kesehatan Wanita'],
+        ];
+
+        // Masukkan data ke dalam tabel 'polis'
+        Poli::insert($polis);
     }
 }
